264f04f8d779fefa2455e67ce34d69c78589e15b
John Epperson <john.epperson@rockagile.io>
2019-07-06 14:12:25 -0400

    Version bump to 0.1.3

--- bootstrappers/chef/lib/shiplane/bootstrappers/chef/version.rb
+++ bootstrappers/chef/lib/shiplane/bootstrappers/chef/version.rb
@@ -3,7 +3,7 @@
 module Shiplane
   module Bootstrappers
     module Chef
-      VERSION = "0.1.2"
+      VERSION = "0.1.3"
     end
   end
 end
--- deployers/capistrano_docker/lib/shiplane/deployers/capistrano_docker/version.rb
+++ deployers/capistrano_docker/lib/shiplane/deployers/capistrano_docker/version.rb
@@ -3,7 +3,7 @@
 module Shiplane
   module Deployers
     module CapistranoDocker
-      VERSION = "0.1.2"
+      VERSION = "0.1.3"
     end
   end
 end
